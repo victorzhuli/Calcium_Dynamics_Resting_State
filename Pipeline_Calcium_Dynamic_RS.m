3802fee12a54d23ca8db4f17dd73b215b17c6fa0
victorzhuli <zhulivictor@gmail.com>
2017-06-19 21:33:19 -0400

    ""

--- Pipeline_Calcium_Dynamic_RS.m
+++ Pipeline_Calcium_Dynamic_RS.m
@@ -1,2 +1,29 @@
 %%%% Study the dynamics of resting state, prepared for SPIE Conference
 %%%% 2017 in San Diego.
+%%
+mouse_id = 1;
+exp_session = 'am';
+%% Load HbO and HbR signals
+mouse_name = sprintf('GC6f_emx_%02d',mouse_id);
+
+if (mouse_id == 01 || mouse_id == 02 || mouse_id == 03) &&  strcmp(exp_session, 'am')
+    folder_name = '150421am GC6-emx 1-3 spont';
+elseif (mouse_id == 01 || mouse_id == 02 || mouse_id == 03) &&  strcmp(exp_session, 'pm')
+    folder_name = '150421pm GC6-emx 1-3 spont';
+elseif (mouse_id == 04 || mouse_id == 05 || mouse_id == 06) &&  strcmp(exp_session, 'am')
+    folder_name = '150609am GC6-emx 4-6 spont';
+else
+    folder_name = '150609pm GC6-emx 4-6 spont';
+end
+
+%--------------------------------------------------------------------------
+if ispc
+    
+elseif ismac
+    load(['/Users/lizhu/Dropbox/GCaMP6f spont and tone reward/', ...
+        folder_name,'/',mouse_name,'/Ca.mat']);
+elseif isunix
+    
+end
+%--------------------------------------------------------------------------
+
