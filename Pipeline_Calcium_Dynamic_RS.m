2487cdffd37607cc12e1f2bd6aeeed41940c1e23
victorzhuli <zhulivictor@gmail.com>
2017-07-05 21:15:50 -0400

    After permutation testing

--- Pipeline_Calcium_Dynamic_RS.m
+++ Pipeline_Calcium_Dynamic_RS.m
@@ -52,8 +52,7 @@ for mouse_id = 1:6
 end
 
 %% combine 'am' and 'pm' for both NA label and WC pattern
-%%
-%%%% unfortunately: out of memory!!
+%% unfortunately: out of memory!!
 lz_combine_RS_ind_wc            
 
 %% based on the computed wavelet coherence and NA/WA borders, pool index for NA/WA scalegrams
@@ -66,7 +65,42 @@ for mouse_id = 1:6
     end
 end
 
-%% based on the pool index for NA/WA scalegrams, compute mean, std, and t-values across NW and WA
+%% save NA and WA wavelet coherence separately
+exp_session_array = {'am', 'pm'}; 
+for mouse_id = 1:6
+    for iexp_session = 1:2
+        exp_session = exp_session_array{iexp_session};  
+        lz_save_separate_wc_NW_WA(mouse_id, exp_session)
+    end
+end
+
+%% based on the pool index for NA/WA scalegrams, conduct permutation test across subjects for NW vs. WA
+%% exclude mouse 3 pm !!!!!!! becasue of sparse WA
+pVal = 0.05;
+[wc_permut_pval, wc_permut_t_orig, wc_permut_crit_t, wc_permut_est_alpha] ...
+    = lz_permutation_NW_WA_2(pVal);
+
+%%%% take entire frequency bins, decide significant changes 
+nCh = 30; nChPair = nCh*(nCh-1)/2;
+% initial
+pVal_th = ones(nChPair, 1);
+for iChPair = 1:nChPair
+    if min(wc_permut_t_orig(:,iChPair)>0.05)
+        pVal_th(iChPair) = 0;
+    end
+end
+        
+        
+load('f');
+figure(12); clf; imagesc(1:nChPair, f, wc_permut_t_orig); 
+xlabel('Channel Pair'); ylabel('Frequency (Hz)'); title('Permutation Test Result (t-val)')
+set(gca, 'fontsize', 20, 'linew',2);
+colorbar
+
+%% 
+
+
+%% based on the pool index for NA/WA scalegrams, compute mean, std, and t-values (using t-test and permutation test) across NW and WA
 exp_session_array = {'am', 'pm'}; 
 for mouse_id = 1:6
     for iexp_session = 1:2
@@ -83,7 +117,7 @@ end
 %%=========================================================================
 %% Check point for wavelet coherence --------------------------------------
 %% plot two raw calcium signals and their wavelet coherence
-mouse_id = 1; exp_session = 'am'; iTr = 5; iCh = 1; jCh = iCh+18;
+mouse_id = 3; exp_session = 'pm'; iTr = 5; iCh = 1; jCh = iCh+5;
 lz_checkpoint_raw_calcium_pair_wc(mouse_id, exp_session, iTr, iCh, jCh);
 
 % Check point for non-whisking labeling ----------------------------------
@@ -94,7 +128,7 @@ mouse_id = 6; exp_session = 'am'; iTr = 4;
 lz_checkpoint_WA_labeling(mouse_id, exp_session, iTr);
 
 %% Check point for mean, std, and t-values for wavelet coherence across NW/WA
-mouse_id = 4; exp_session = 'am';
+mouse_id = 2; exp_session = 'am';
 lz_checkpoint_tval_wc_NW_WA(mouse_id, exp_session);
 
 
--- lz_build_folder_name.m
+++ lz_build_folder_name.m
@@ -1,6 +1,8 @@
 %%%% 06/22/2017
 %%%% construct folder name and mouse name for loading spontaneous mouse data
-function [folder_name, mouse_name, loadName_RS_ind, loadName_wc, loadName_WA_ind, loadName_wc_RS, loadName_wc_WA, loadName_RS_ind_pool, loadName_WA_ind_pool, loadName_wc_stat] ...
+function [folder_name, mouse_name, loadName_RS_ind, loadName_wc, loadName_WA_ind, ...
+    loadName_wc_RS, loadName_wc_WA, loadName_RS_ind_pool, loadName_WA_ind_pool, ...
+    loadName_wc_stat, loadName_wc_separate] ...
     = lz_build_folder_name(mouse_id, exp_session)
 
 mouse_name = sprintf('GC6f_emx_%02d',mouse_id);
@@ -12,7 +14,7 @@ loadName_WA_ind = sprintf('GC6f_emx_%02d_%2s_spont_whisking_active_index', mouse
 loadName_wc_RS = sprintf('GC6f_emx_%02d_%2s_spont_resting_state_wc_RS', mouse_id, exp_session);
 loadName_wc_WA = sprintf('GC6f_emx_%02d_%2s_spont_resting_state_wc_WA', mouse_id, exp_session);
 loadName_wc_stat = sprintf('GC6f_emx_%02d_%s_wc_stat_NW_WA', mouse_id, exp_session);
-
+loadName_wc_separate = sprintf('GC6f_emx_%02d_%s_wc_separate_NW_WA', mouse_id, exp_session);
 
 if (mouse_id == 01 || mouse_id == 02 || mouse_id == 03) &&  strcmp(exp_session, 'am')
     folder_name = '150421am GC6-emx 1-3 spont';
